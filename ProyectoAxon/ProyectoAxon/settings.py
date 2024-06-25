04c61cd0775cdb8938a5d8db087ed1d70095e6c7
fcchidan <fcchidanmail.com>
2024-06-25 12:47:42 -0400

    combio del host

--- ProyectoAxon/ProyectoAxon/settings.py
+++ ProyectoAxon/ProyectoAxon/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'NAME': 'axondb',
         'USER': 'axon',
         'PASSWORD': 'Axon2024db!',
-        'HOST': 'axon-426916:northamerica-northeast1:axondb',
+        'HOST': '34.152.57.93',
         'PORT': '3306',
     }
 }
